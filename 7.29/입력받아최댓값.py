827cfd08c6ae303f0a800a89fcb65492c8c62574
LWJOO <110073600+LWJOO@users.noreply.github.com>
2022-07-29 12:18:23 +0900

    Update 입력받아최댓값.py

--- 7.29/입력받아최댓값.py
+++ 7.29/입력받아최댓값.py
@@ -1,6 +1,6 @@
 print('리스트 구분은 띄어쓰기로 합니다. ')
 x = list(input('리스트를 입력하세요 : ').split())
-
+listx = list(map(int, x))
 def seq_search_sentinel(a, key):
     b = a.copy()
     b.append(key)
@@ -21,10 +21,10 @@ def max_of(a):
             maximum = a[i]
     return maximum
 
-y = max_of(x)
-index = seq_search_sentinel(x, y)
+y = max_of(listx)
+index = seq_search_sentinel(listx, y)
 print(f'최댓값은 {y}입니다.')
 if index == -1:
     print('검색값을 갖는 원소가 존재하지 않습니다.')
 else:
-    print(f'최댓값은 a[{index}]에 있습니다.')
+    print(f'최댓값은 리스트[{index}]에 있습니다.')
